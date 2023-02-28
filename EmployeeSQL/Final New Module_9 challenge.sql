9c37d1836a5a5b23fa29a00ba5a12b357d5ca784
Alejandro-Delacruz <118419015+Alejandro-Delacruz@users.noreply.github.com>
2023-02-27 22:29:51 -0600

    Update Final New Module_9 challenge.sql

--- EmployeeSQL/Final New Module_9 challenge.sql
+++ EmployeeSQL/Final New Module_9 challenge.sql
@@ -1,17 +1,17 @@
 /*List the employee number, last name, first name, sex,
 and salary of each employee (2 points)*/
 
-SELECT		s.emp_no,last_name,first_name,sex,salary 
-FROM 		employees e
-LEFT JOIN 	salaries s
-ON 			e.emp_no=s.emp_no;
+SELECT    s.emp_no,last_name,first_name,sex,salary 
+FROM      employees e
+LEFT JOIN alaries s
+ON 			  e.emp_no=s.emp_no;
 
 /*List the first name, last name, and hire date for the 
 employees who were hired in 1986 (2 points)*/
 
-SELECT 		first_name,last_name, hire_date 
-FROM 		employees
-WHERE EXTRACT(YEAR FROM hire_date)=1986;
+SELECT    first_name,last_name, hire_date 
+FROM 		  employees
+WHERE     EXTRACT(YEAR FROM hire_date)=1986;
 
 /*List the manager of each department along with their department number, 
 department name, employee number, last name, and first name (2 points)*/
@@ -40,9 +40,9 @@ FROM 	emp_dept;
 /*List first name, last name, and sex of each employee whose first name 
 is Hercules and whose last name begins with the letter B (2 points)*/
 
-SELECT		first_name,last_name,sex
-FROM		employees
-WHERE		first_name='Hercules' AND SUBSTRING(last_name,1,1)='B';
+SELECT    first_name,last_name,sex
+FROM      employees
+WHERE		  first_name='Hercules' AND SUBSTRING(last_name,1,1)='B';
 
 /*
 List each employee in the Sales department, 
@@ -77,7 +77,7 @@ WHERE dept_name='Sales' OR dept_name='Development';
 /*List the frequency counts, in descending order, 
 of all the employee last names (that is, how many employees share each last name) (4 points)*/
 
-SELECT 		last_name,COUNT(first_name) AS frequency
-FROM 		employees
+SELECT    last_name,COUNT(first_name) AS frequency
+FROM      employees
 GROUP BY 	last_name
 ORDER BY 	frequency desc;
